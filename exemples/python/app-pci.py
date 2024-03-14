34033cb86c1b27d61e6a581726c02e4a2d0001e4
Rafael-Azvedo <112179732+Azvedo@users.noreply.github.com>
2024-03-14 16:02:40 -0300

    Update app-pci.py
    testando mapeamento

--- exemples/python/app-pci.py
+++ exemples/python/app-pci.py
@@ -17,26 +17,41 @@ def main():
         print("Syntax: %s </dev/device_file>"%sys.argv[0])
         exit(1)
 
-    fd = os.open(sys.argv[1], os.O_RDWR)
+    fd = os.open("/dev/mydev", os.O_RDWR)
+    #data = 0b0
+    data1 = 0b010000000
+    ioctl(fd, WR_GREEN_LEDS)
+    retval = os.write(fd, data1.to_bytes(4, 'little'))
+    
+    #data2 = 0b111000111000111000  
+    #ioctl(fd, WR_RED_LEDS)
+    #retval = os.write(fd, data2.to_bytes(4, 'little'))
+    
+    
+    #data = 0x40404040;
+    #data1 = 0x79243019;
+    #ioctl(fd, WR_L_DISPLAY)
+    #retval = os.write(fd, data1.to_bytes(4, 'little'))
+    #print("wrote %d bytes"%retval)
+    #data2 = 0x12027800;
+    #ioctl(fd, WR_R_DISPLAY)
+    #retval = os.write(fd, data2.to_bytes(4, 'little'))
+    #print("wrote %d bytes"%retval)
 
-    # data to write
-    data = 0x40404079;
-    ioctl(fd, WR_R_DISPLAY)
-    retval = os.write(fd, data.to_bytes(4, 'little'))
-    print("wrote %d bytes"%retval)
-
-    # data to write
-    data = 0x79404040;
-    ioctl(fd, WR_L_DISPLAY)
-    retval = os.write(fd, data.to_bytes(4, 'little'))
-    print("wrote %d bytes"%retval)
-
+	
     ioctl(fd, RD_PBUTTONS)
     red = os.read(fd, 4); # read 4 bytes and store in red var
-    print("red 0x%X"%int.from_bytes(red, 'little'))
+    #print("button 0x%X"%int.from_bytes(red, 'little'))
+
+    ioctl(fd, RD_SWITCHES)
+    rad = os.read(fd, 4); # read 4 bytes and store in rad var
+    #print("switch 0x%X"%int.from_bytes(rad, 'little'))
+    
+    print(f"button: {int(bin(int.from_bytes(red, 'little')), 2)} | switches: {bin(int.from_bytes(rad, 'little'))}")
 
     os.close(fd)
 
 if __name__ == '__main__':
-    main()
+    while True:
+        main()
 
