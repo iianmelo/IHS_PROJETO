27e513c7a96b698a388da7ff29dcddeca036b3fb
Rafael-Azvedo <112179732+Azvedo@users.noreply.github.com>
2024-03-15 17:56:24 -0300

    corrigindo 2 (binarios)

--- src/controls/display_control.py
+++ src/controls/display_control.py
@@ -3,16 +3,16 @@ from fcntl import ioctl
 
 WR_L_DISPLAY = 24931
 WR_R_DISPLAY = 24932
-hex0 = 0b1111110
-hex1 = 0b0110000
-hex2 = 0b1101101
-hex3 = 0b1111001
-hex4 = 0b0110011
-hex5 = 0b1011011
-hex6 = 0b1011111
-hex7 = 0b1110000
-hex8 = 0b1111111
-hex9 = 0b1111011
+hex0 = 0b11000000
+hex1 = 0b11111001
+hex2 = 0b10100100
+hex3 = 0b10110000
+hex4 = 0b10011001
+hex5 = 0b10010010
+hex6 = 0b10000010
+hex7 = 0b11111000
+hex8 = 0b10000000
+hex9 = 0b10010000
 numbers = [hex0, hex1, hex2, hex3, hex4, hex5, hex6, hex7, hex8, hex9]
 
 #display direito vai ser usado para um numero de 4 digitos
