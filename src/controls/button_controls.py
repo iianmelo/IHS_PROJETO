62f4b538d830b17173481557906414bc6702daaa
Rafael-Azvedo <112179732+Azvedo@users.noreply.github.com>
2024-03-15 18:59:57 -0300

    padronização dos botões

--- src/controls/button_controls.py
+++ src/controls/button_controls.py
@@ -11,9 +11,14 @@ fd = os.open("/dev/mydev", os.O_RDWR)
 def read_button ():
     ioctl(fd, RD_PBUTTONS)
     button = os.read(fd, 4)
-    return {int(bin(int.from_bytes(button, 'little')), 2)} # read 4 bytes and store in red var
+    read = int(bin(int.from_bytes(button, 'little')), 2)
+    if(read == 7): return 1
+    elif(read == 11): return 2
+    elif(read == 13): return 3
+    elif(read == 14): return 4
+    else: return 0
     
 def read_switchs():
     ioctl(fd, RD_SWITCHES)
     switch = os.read(fd, 4) # read 4 bytes and store in rad var
-    return {int(bin(int.from_bytes(switch, 'little')), 2)}
+    return int(bin(int.from_bytes(switch, 'little')), 2)
